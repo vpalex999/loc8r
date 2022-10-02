53fc7df0137ccc7d06190ec666cf9e918da3824c
vpalex999 <vpalex@mail.ru>
2022-10-02 08:26:33 +0500

    add static views

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,14 +1,14 @@
 // Получить (GET) домашнюю страницу
 const homelist = function (req, res) {
-  res.render('index', { title: 'Home' });
+  res.render('locations-list', { title: 'Home' });
 };
 
 const locationInfo = function (req, res) {
-  res.render('index', { title: 'Location Info' });
+  res.render('location-info', { title: 'Location Info' });
 };
 
 const addReview = function (req, res) {
-  res.render('index', { title: 'Add review' });
+  res.render('location-review-form', { title: 'Add review' });
 };
 
 module.exports = { homelist, locationInfo, addReview };
