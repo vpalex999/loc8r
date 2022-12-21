ec68a97468d2795556cce3185bb311f215a02a66
vpalex999 <vpalex@mail.ru>
2022-12-21 19:44:00 +0500

    4.5.6

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -32,7 +32,37 @@ const homelist = function (req, res) {
 };
 
 const locationInfo = function (req, res) {
-  res.render('location-info', { title: 'Location Info' });
+  res.render('location-info', {
+    title: 'Location Info',
+    location: {
+      name: 'Starcups',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 3,
+      openingHours: [
+        'Monday - Friday : 7:00am - 7:00pm',
+        'Saturday : 8:00am - 5:00pm',
+        'Sunday : closed'
+      ],
+      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+      locationMap: 'http://maps.googleapis.com/maps/api/staticmap?center=51.455041,-0.9690884&zoom=17&size=400x350&sensor=false&markers=51.455041,-0.9690884&scale=2',
+      customerReviews: [
+        {
+          rating: 5,
+          author: 'Simon Holmes',
+          timeStamp: '16 July 2013',
+          text: "What a great place. I can't say enough good things about it.",
+        },
+        {
+          rating: 3,
+          author: 'Charlie Chaplin',
+          timeStamp: '17 June 2013',
+          text: "It was okay. Coffee wasn't great, but the wifi was fast.",
+        }
+      ],
+      distance: '100m',
+    },
+
+  });
 };
 
 const addReview = function (req, res) {
