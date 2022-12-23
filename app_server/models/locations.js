8ae11885536089c853b376951cc1875ff67ff6ea
vpalex999 <vpalex@mail.ru>
2022-12-23 19:25:02 +0500

    5.3.3

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -1,9 +1,25 @@
 var mongoose = require('mongoose');
 
+var openingTimeSchema = new mongoose.Schema({
+  days: { type: String, required: true },
+  opening: String,
+  closing: String,
+  closed: { type: Boolean, required: true }
+});
+
+var customerReviewSchema = new mongoose.Schema({
+  author: String,
+  rating: { type: Number, 'default': 0, min: 0, max: 5 },
+  text: String,
+  timeStamp: { type: Date, 'default': Date.now }
+});
+
 var locationSchema = new mongoose.Schema({
   name: { type: String, required: true },
   address: String,
   rating: { type: Number, 'default': 0, min: 0, max: 5 },
   facilities: [String],
-  coords: { type: [Number], index: '2dsphere' }
+  coords: { type: [Number], index: '2dsphere' },
+  openingHours: [openingTimeSchema],
+  customerReviews: [customerReviewSchema]
 });
